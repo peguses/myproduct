d8a144d98a498fd244b6b3dec9f68069de445c11
Jayan Wadusinghearachchi <jayan.2029@gmal.com>
2021-06-23 18:17:23 +0530

    init commit

---
+++ src/test/java/com/myproduct/myproduct/MyproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproduct.myproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyproductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
